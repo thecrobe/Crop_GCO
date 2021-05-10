6c83c7003ee60b551a03f17e13108fb028239744
Justin Stewart <justinstewart@MacBook-Pro-3.local>
2021-05-10 11:32:38 +0200

    Soybean GBIF

---
+++ SDMs/SDM_GBIF_Soybean.R
@@ -0,0 +1,151 @@
+library(rgbif)
+library(dplyr)
+library(maps)
+library(mapdata)
+library(dismo) 
+library(rJava) 
+library(maptools)
+library(jsonlite)
+library(ggplot2)
+library(RVAideMemoire)
+# Collect occurrences for pests
+# https://plantvillage.psu.edu/topics/soybean/infos/diseases_and_pests_description_uses_propagation
+
+#Pseudomonas syringae
+Pseudomonas.syringae.gbf<-occ_search(scientificName = "Pseudomonas syringae",hasCoordinate = TRUE, limit=100000)
+Pseudomonas.syringae<-data.frame(Pseudomonas.syringae.gbf$data)
+# Number of occurrences
+dim(Pseudomonas.syringae) #483 
+# Coordinates
+Pseudomonas.syringae.xy<-Pseudomonas.syringae %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+#Xanthomonas campestris
+Pseudomonas.syringae.gbf<-occ_search(scientificName = "Pseudomonas syringae",hasCoordinate = TRUE, limit=100000)
+Pseudomonas.syringae<-data.frame(Pseudomonas.syringae.gbf$data)
+# Number of occurrences
+dim(Pseudomonas.syringae) #1223 
+# Coordinates
+Pseudomonas.syringae.xy<-Pseudomonas.syringae %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+#Xanthomonas campestris
+Xanthomonas.campestris.gbf<-occ_search(scientificName = "Xanthomonas campestris",hasCoordinate = TRUE, limit=100000)
+Xanthomonas.campestris<-data.frame(Xanthomonas.campestris.gbf$data)
+# Number of occurrences
+dim(Xanthomonas.campestris) #577
+# Coordinates
+Xanthomonas.campestris.xy<-Xanthomonas.campestris %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Rhizoctonia solani
+Rhizoctonia.solani.gbf<-occ_search(scientificName = "Rhizoctonia solani",hasCoordinate = TRUE, limit=100000)
+Rhizoctonia.solani<-data.frame(Rhizoctonia.solani.gbf$data)
+# Number of occurrences
+dim(Rhizoctonia.solani) #1270
+# Coordinates
+Rhizoctonia.solani.xy<-Rhizoctonia.solani %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Phakopsora pachyrhizi - TOO FEW
+#Phakopsora.pachyrhizi.gbf<-occ_search(scientificName = "Phakopsora.pachyrhizi ",hasCoordinate = TRUE, limit=100000)
+#Phakopsora.pachyrhizi<-data.frame(Phakopsora.pachyrhizi.gbf$data)
+# Number of occurrences
+#dim(Phakopsora.pachyrhizi) #268
+# Coordinates
+#Phakopsora.pachyrhizi.xy<-Phakopsora.pachyrhizi %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Sclerotinia clerotiorum - Not In Database
+#Sclerotinia.clerotiorum.gbf<-occ_search(scientificName = "Sclerotinia clerotiorum",hasCoordinate = TRUE, limit=100000)
+#Sclerotinia.clerotiorum<-data.frame(Sclerotinia.clerotiorum.gbf$data)
+# Number of occurrences
+#dim(Sclerotinia.clerotiorum) #483 
+# Coordinates
+#Sclerotinia.clerotiorum.xy<-Sclerotinia.clerotiorum %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Septoria glycines - TOO FEW
+#Septoria.glycines.gbf<-occ_search(scientificName = "Septoria glycines",hasCoordinate = TRUE, limit=100000)
+#Septoria.glycines<-data.frame(Septoria.glycines.gbf$data)
+# Number of occurrences
+#dim(Septoria.glycines) #5
+# Coordinates
+#Septoria.glycines.xy<-Septoria.glycines %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Spodoptera exigua
+Spodoptera.exigua.gbf<-occ_search(scientificName = "Spodoptera exigua",hasCoordinate = TRUE, limit=100000)
+Spodoptera.exigua<-data.frame(Spodoptera.exigua.gbf$data)
+# Number of occurrences
+dim(Spodoptera.exigua) #8338
+# Coordinates
+Spodoptera.exigua.xy<-Spodoptera.exigua %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Spodoptera praefica - TOO FEW
+#Spodoptera.praefica.gbf<-occ_search(scientificName = "Spodoptera praefica",hasCoordinate = TRUE, limit=100000)
+#Spodoptera.praefica<-data.frame(Spodoptera.praefica.gbf$data)
+# Number of occurrences
+#dim(Spodoptera.praefica) #4379
+# Coordinates
+#Spodoptera.praefica.xy<-Spodoptera.praefica %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Acalymma vittata - NOT IN DATABASE
+#Acalymma.vittata.gbf<-occ_search(scientificName = "Acalymma vittata",hasCoordinate = TRUE, limit=100000)
+#Acalymma.vittata<-data.frame(Acalymma.vittata.gbf$data)
+# Number of occurrences
+#dim(Acalymma.vittata) #4379
+# Coordinates
+#Acalymma.vittata.xy<-Acalymma.vittata %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Diabrotica undecimpunctata
+Diabrotica.undecimpunctata.gbf<-occ_search(scientificName = "Diabrotica undecimpunctata",hasCoordinate = TRUE, limit=100000)
+Diabrotica.undecimpunctata<-data.frame(Diabrotica.undecimpunctata.gbf$data)
+# Number of occurrences
+dim(Diabrotica.undecimpunctata) #11919
+# Coordinates
+Diabrotica.undecimpunctata.xy<-Diabrotica.undecimpunctata %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Epilachna svarivestis - TOO FEW
+#Epilachna.varivestis.gbf<-occ_search(scientificName = "Epilachna varivestis",hasCoordinate = TRUE, limit=100000)
+#Epilachna.varivestis<-data.frame(Epilachna.varivestis.gbf$data)
+# Number of occurrences
+#dim(Epilachna.varivestis) #459
+# Coordinates
+#Epilachna.varivestis.xy<-Epilachna.varivestis %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Septoria glycines - TOO FEW
+#Septoria.glycines.gbf<-occ_search(scientificName = "Septoria glycines",hasCoordinate = TRUE, limit=100000)
+#Septoria.glycines<-data.frame(Septoria.glycines.gbf$data)
+# Number of occurrences
+#dim(Septoria.glycines) #4379
+# Coordinates
+#Septoria.glycines.xy<-Septoria.glycines %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Cercospora kikuchii - NOT IN DATABASE
+#Cercospora.kikuchii.gbf<-occ_search(scientificName = "Cercospora kikuchii",hasCoordinate = TRUE, limit=100000)
+#Cercospora.kikuchii<-data.frame(Cercospora.kikuchii.gbf$data)
+# Number of occurrences
+#dim(Cercospora.kikuchii) #4379
+# Coordinates
+#Cercospora.kikuchii.xy<-Cercospora.kikuchii %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Macrophomina phaseolina - TOO FEW
+#Macrophomina.phaseolina.gbf<-occ_search(scientificName = "Macrophomina phaseolina",hasCoordinate = TRUE, limit=100000)
+#Macrophomina.phaseolina<-data.frame(Macrophomina.phaseolina.gbf$data)
+# Number of occurrences
+#dim(Macrophomina.phaseolina) #235 
+# Coordinates
+#Macrophomina.phaseolina.xy<-Macrophomina.phaseolina %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Cercospora sojina - TOO FEW
+#Cercospora.sojina.gbf<-occ_search(scientificName = "Cercospora sojina",hasCoordinate = TRUE, limit=100000)
+#Cercospora.sojina<-data.frame(Cercospora.sojina.gbf$data)
+# Number of occurrences
+#dim(Cercospora.sojina) #10
+# Coordinates
+#Cercospora.sojina.xy<-Cercospora.sojina %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+# Phytophthora megasperma - TOO FEW
+#Phytophthora.megasperma.gbf<-occ_search(scientificName = "Phytophthora megasperma",hasCoordinate = TRUE, limit=100000)
+#Phytophthora.megasperma<-data.frame(Phytophthora.megasperma.gbf$data)
+# Number of occurrences
+#dim(Phytophthora.megasperma) #50
+# Coordinates
+#Phytophthora.megasperma.xy<-Phytophthora.megasperma %>% dplyr::select(scientificName,decimalLongitude, decimalLatitude)
+
+
+
