c948a98b5851577ae4d11487f92b6da9dec76eca
thecrobe <justin618s@gmail.com>
2021-11-03 15:08:04 +0100

    halfway spatial and null

--- NewAnalyses/SpatMod_Iterate.R
+++ NewAnalyses/SpatMod_Iterate.R
@@ -2,8 +2,6 @@ library(spdep)
 library(spatialreg)
 library(rgdal)
 library(ggplot2)
-library(foreach)
-library(doParallel)
 library(rgeos)
 library(dplyr)
 
@@ -16,37 +14,156 @@ fishnet<- readOGR(dsn= "GIS/", layer="Fishnet_yield_NoAntarctica")
 fishnet.barley<-subset(fishnet, fishnet$mean_barle > 0 ) #yield > 0 
 fishnet.barley$logBarley<-log10(fishnet.barley$mean_barle)
 xy <- coordinates(fishnet.barley)
-xy1<-data.frame(xy)
-barleyGCO<-readOGR(dsn = "GIS/",layer="BarleyWheat_GC")
-barleyGCOcentroid<-gCentroid(barleyGCO)
 
 #Neighborhoods
-nb <- poly2nb(fishnet.barley)
-lw <- nb2listw(nb,zero.policy = T)
-print(lw)
+nb <- poly2nb(fishnet.barley,queen = TRUE)
+lw <- nb2listw(nb,zero.policy = TRUE,style = "W")
 
 #Register cluster
-cl <- parallel::makeCluster(2)
-doParallel::registerDoParallel(cl)
+library(RhpcBLASctl)
+blas_set_num_threads(8)
 N=100 #sample size
-output <- data.frame(slope=numeric(N), pvalue=numeric(N)m x = numeric(N), y = numeric(N), moran_p=numeric(N)
-iteration<-foreach(i = 1:N) %dopar% {
-  #load package 
-  library(dplyr)
-  library(sp) #try :: 
-  library(spdep)
+output <- data.frame(slope=numeric(N), pval=numeric(N), x=numeric(N), y=numeric(N), moran_i=numeric(N), moran_p=numeric(N))
+iteration<-for(i in 1:N){
   # pick a point
   random.point <- sample(1:nrow(xy), 1)
   # calculate distances from that point
-  distances <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  dist <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  fishnet.barley$distances<-dist/(1000*1000)
   #fit model 
-  barley.sper<-errorsarlm(logBarley ~ distances, data=fishnet.barley@data, lw, tol.solve=1.0e-30,zero.policy  = TRUE)
-  sa<-moran.mc(merged$residuals, lw, 1000, zero.policy = T)
+  barley.sper<-errorsarlm(logBarley ~ distances, listw = lw, data = fishnet.barley, quiet=TRUE,zero.policy = TRUE)
+  sumr<-summary(barley.sper)
+  sa<-moran.mc(barley.sper$residuals, lw, 1000, zero.policy = T)
   # associate it to the point
-  output[i,] <- c(coef(barley.sper)[3], #PVALUE, xy[random.point,][1],xy[random.point,][2], #MoranPvalue)
-
+  output[i,] <- c(coef(barley.sper)[3],sumr$Wald1[3], xy[random.point,][1],xy[random.point,][2], moran$p.value)
 }
-stopCluster(cl)
-x<-data.frame(do.call(rbind,iteration))
+gc()
+ggplot(output, aes(x=output$x,y=output$y,color=log10(output$slope))) +geom_point()
+write.csv(x = output, file="NewAnalyses/Null/barley_null.csv")
 
-ggplot(x, aes(x=x$V2,y=x$V3,color=x$rescale_ND)) +geom_point()
+#Cassava data prep
+fishnet.cassava<-subset(fishnet, fishnet$mean_cassa > 0 ) #yield > 0 
+fishnet.cassava$logCassava<-log10(fishnet.cassava$mean_cassa)
+xy <- coordinates(fishnet.cassava)
+
+#Neighborhoods
+nb <- poly2nb(fishnet.cassava,queen = TRUE)
+lw <- nb2listw(nb,zero.policy = TRUE,style = "W")
+
+#Register cluster
+library(RhpcBLASctl)
+blas_set_num_threads(8)
+N=100 #sample size
+output <- data.frame(slope=numeric(N), pval=numeric(N), x=numeric(N), y=numeric(N), moran_i=numeric(N), moran_p=numeric(N))
+iteration<-for(i in 1:N){
+  # pick a point
+  random.point <- sample(1:nrow(xy), 1)
+  # calculate distances from that point
+  dist <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  fishnet.cassava$distances<-dist/(1000*1000)
+  #fit model 
+  cassava.sper<-errorsarlm(logCassava ~ distances, listw = lw, data = fishnet.cassava, quiet=TRUE,zero.policy = TRUE)
+  sumr<-summary(cassava.sper)
+  sa<-moran.mc(cassava.sper$residuals, lw, 1000, zero.policy = T)
+  # associate it to the point
+  output[i,] <- c(coef(cassava.sper)[3],sumr$Wald1[3], xy[random.point,][1],xy[random.point,][2], moran$p.value)
+}
+gc()
+ggplot(output, aes(x=output$x,y=output$y,color=log10(output$slope))) +geom_point()
+write.csv(x = output, file="NewAnalyses/Null/cassava_null.csv")
+
+
+#Groundnut data prep
+fishnet.groundnut<-subset(fishnet, fishnet$mean_groun > 0 ) #yield > 0 
+fishnet.groundnut$logGroundnut<-log10(fishnet.groundnut$mean_groun)
+xy <- coordinates(fishnet.groundnut)
+
+#Neighborhoods
+nb <- poly2nb(fishnet.groundnut,queen = TRUE)
+lw <- nb2listw(nb,zero.policy = TRUE,style = "W")
+
+#Register cluster
+library(RhpcBLASctl)
+blas_set_num_threads(8)
+N=100 #sample size
+output <- data.frame(slope=numeric(N), pval=numeric(N), x=numeric(N), y=numeric(N), moran_i=numeric(N), moran_p=numeric(N))
+iteration<-for(i in 1:N){
+  # pick a point
+  random.point <- sample(1:nrow(xy), 1)
+  # calculate distances from that point
+  dist <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  fishnet.groundnut$distances<-dist/(1000*1000)
+  #fit model 
+  groundnut.sper<-errorsarlm(logGroundnut ~ distances, listw = lw, data = fishnet.groundnut, quiet=TRUE,zero.policy = TRUE)
+  sumr<-summary(groundnut.sper)
+  sa<-moran.mc(groundnut.sper$residuals, lw, 1000, zero.policy = T)
+  # associate it to the point
+  output[i,] <- c(coef(groundnut.sper)[3],sumr$Wald1[3], xy[random.point,][1],xy[random.point,][2], sa$p.value)
+}
+gc()
+ggplot(output, aes(x=output$x,y=output$y,color=log10(output$slope))) +geom_point()
+write.csv(x = output, file="NewAnalyses/Null/groundnut_null.csv")
+
+#Maize data prep
+fishnet.maize<-subset(fishnet, fishnet$mean_maize > 0 ) #yield > 0 
+fishnet.maize$logMaize<-log10(fishnet.maize$mean_maize)
+xy <- coordinates(fishnet.maize)
+
+#Neighborhoods
+nb <- poly2nb(fishnet.maize,queen = TRUE)
+lw <- nb2listw(nb,zero.policy = TRUE,style = "W")
+
+#Register cluster
+library(RhpcBLASctl)
+blas_set_num_threads(8)
+N=100 #sample size
+output <- data.frame(slope=numeric(N), pval=numeric(N), x=numeric(N), y=numeric(N), moran_i=numeric(N), moran_p=numeric(N))
+iteration<-for(i in 1:N){
+  # pick a point
+  random.point <- sample(1:nrow(xy), 1)
+  # calculate distances from that point
+  dist <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  fishnet.maize$distances<-dist/(1000*1000)
+  #fit model 
+  maize.sper<-errorsarlm(logMaize ~ distances, listw = lw, data = fishnet.maize, quiet=TRUE,zero.policy = TRUE)
+  sumr<-summary(maize.sper)
+  sa<-moran.mc(maize.sper$residuals, lw, 1000, zero.policy = T)
+  # associate it to the point
+  output[i,] <- c(coef(maize.sper)[3],sumr$Wald1[3], xy[random.point,][1],xy[random.point,][2], moran$p.value)
+}
+gc()
+ggplot(output, aes(x=output$x,y=output$y,color=log10(output$slope))) +geom_point()
+write.csv(x = output, file="NewAnalyses/Null/maize_null.csv")
+
+
+#Rapeseed data prep
+fishnet.Rapeseed<-subset(fishnet, fishnet$mean_rapes > 0 ) #yield > 0 
+fishnet.Rapeseed$logRapeseed<-log10(fishnet.Rapeseed$mean_rapes)
+xy <- coordinates(fishnet.Rapeseed)
+
+#Neighborhoods
+nb <- poly2nb(fishnet.Rapeseed,queen = TRUE)
+lw <- nb2listw(nb,zero.policy = TRUE,style = "W")
+
+#Register cluster
+library(RhpcBLASctl)
+blas_set_num_threads(8)
+N=100 #sample size
+output <- data.frame(slope=numeric(N), pval=numeric(N), x=numeric(N), y=numeric(N), moran_i=numeric(N), moran_p=numeric(N))
+iteration<-for(i in 1:N){
+  # pick a point
+  random.point <- sample(1:nrow(xy), 1)
+  # calculate distances from that point
+  dist <- spDistsN1(xy,xy[random.point,], longlat = FALSE)
+  fishnet.Rapeseed$distances<-dist/(1000*1000)
+  #fit model 
+  Rapeseed.sper<-errorsarlm(logRapeseed ~ distances, listw = lw, data = fishnet.Rapeseed, quiet=TRUE,zero.policy = TRUE)
+  sumr<-summary(Rapeseed.sper)
+  moran<-moran.mc(Rapeseed.sper$residuals, lw, 1000, zero.policy = T)
+  # associate it to the point
+  output[i,] <- c(coef(Rapeseed.sper)[3],sumr$Wald1[3], xy[random.point,][1],xy[random.point,][2], moran$p.value)
+}
+gc()
+ggplot(output, aes(x=output$x,y=output$y,color=log10(output$slope))) +geom_point()
+write.csv(x = output, file="NewAnalyses/Null/Rapeseed_null.csv")
+
---
+++ NewAnalyses/SpatialRF/rye_SpatialRF.R
@@ -0,0 +1,510 @@
+
+library(spatialRF)
+library(spdep)
+library(spdplyr)
+library(rgdal)
+library(ggplot2)
+library(ranger)
+
+
+
+#Read In
+#Fishnet- pixel = 100km^2
+fishnet<- readOGR(dsn= "GIS/", layer="Fishnet_yield_NoAntarctica")
+summary(fishnet)
+#Set Projection
+proj4string(fishnet) <- CRS("+init=epsg:3786") 
+
+#Some covariates
+ryemodel<-na.omit(read.csv(file="Models/Rye_RF.csv", header=T))
+mapping<-read.csv(file="Fishnets/GCO_Mapping.csv", header=T)
+mapping$Fishnet_ID<-mapping$FISHNET_ID #Fixing name
+
+#Merging fishnet with covariates
+m <- sp::merge(fishnet, ryemodel, by='Fishnet_ID')
+rye<-sp::merge(m, mapping, by='Fishnet_ID')
+rye.p = subset(rye, mean_rye_Y > 0) #selecting values > 0 
+
+#Transform numerical covariates
+rye.num<-rye.p@data %>% select(mean_rye_Y, AET_mean, rye_Fertilizer, Pesticide,GDP_Mean) 
+rye.scaled<-data.frame(log10(rye.num+1)) #psuedo count to avoid inf
+rye.scaled$FISHNET_ID<-as.numeric(rye.p@data$Fishnet_ID)
+
+#Select categorical covariates
+rye.cat<-rye.p %>% select(COUNTRY.x,RyeGCO, FISHNET_ID,Latitude,Longitude, CONTINENT)
+
+#Count number of pixels per country 
+county.pivot<-rye.cat@data %>% group_by(COUNTRY.x) %>% dplyr::summarize(CountyPixelCount = dplyr::n())
+rye.catagory<-sp::merge(rye.cat,county.pivot, by="COUNTRY.x")
+rye.catagory<-rye.catagory@data
+
+
+########## Asia #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "Asia"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10, 50)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_asia_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_asia_pref.csv")
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance( model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_AsiaVarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"))
+asia.rc<-spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_Asia_RespCurv.csv")
+
+
+########## Africa #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "Africa"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_africa_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_africa_pref.csv")
+
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance( model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_AfricaVarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"))
+africa.rc<-spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_Africa_RespCurv.csv")
+
+
+########## North America #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "North America"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10,50)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_NorthAmerica_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_NorthAmerica_pref.csv")
+
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance(model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_NorthAmerica_VarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables =c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+NorthAm.rc<-spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_NorthAmerica_RespCurv.csv")
+
+
+########## Australia + Oceania #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "Australia" | CONTINENT=="Oceania"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10,50)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_ausocean_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_ausocean_pref.csv")
+
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance( model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_AusOceanVarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"))
+spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_AusOcean_RespCurv.csv")
+
+########## Europe #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "Europe"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10,50)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_europe_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_europe_pref.csv")
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance(model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_EuropeVarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"))
+spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_Europe_RespCurv.csv")
+
+########## South America #########
+#Select Continent
+rye.comb<-merge(rye.catagory,rye.scaled, by="FISHNET_ID")
+rye.final<-(filter((rye.comb),CONTINENT == "South America"))
+rye.final<-na.omit(rye.final)
+
+summary(rye.final)
+rye.final$RyeGCO<-as.factor(rye.final$RyeGCO)
+rye.final$COUNTRY.x<-as.factor(rye.final$COUNTRY.x)
+rye.coords<-rye.final %>% select(Latitude,Longitude) #select coordinates
+
+#Create distance matrix
+dist.mat <- data.matrix(dist(cbind(rye.coords$Latitude, rye.coords$Longitude)))
+dim(rye.final) #check dims
+dim(dist.mat) #check dims
+
+#coordinates of the cases
+x<-rye.coords$Latitude
+y<-rye.coords$Longitude
+xy <- data.frame(cbind(x,y))
+
+#distance matrix
+distance.matrix <- dist.mat
+#distance thresholds (same units as distance_matrix)
+distance.thresholds <- c(0, 1, 5, 10,50)
+#random seed for reproducibility
+random.seed <- 1
+# remove data from spatial dataframe
+rye.data<-rye.final
+
+model.non.spatial <- spatialRF::rf(
+  data = rye.data,
+  dependent.variable.name = "mean_rye_Y",
+  predictor.variable.names = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"),
+  distance.matrix = distance.matrix,
+  distance.thresholds = distance.thresholds,
+  xy = xy, #not needed by rf, but other functions read it from the model
+  seed = random.seed,n.cores = 12,
+  verbose = FALSE)
+
+# Moran's Plot
+spatialRF::plot_moran(model.non.spatial, verbose = FALSE)
+
+#Spatial Model
+model.spatial <- spatialRF::rf_spatial(
+  model = model.non.spatial,
+  method = "mem.moran.sequential", #default method
+  verbose = FALSE,
+  seed = random.seed,
+  n.cores = 12
+)
+
+write.csv(x = model.spatial$residuals$values, file="NewAnalyses/SpatialRF/Rye_South America_residuals.csv")
+write.csv(x=model.spatial$performance, file="NewAnalyses/SpatialRF/Rye_South America_pref.csv")
+#Check residuals
+spatialRF::plot_residuals_diagnostics(model.spatial,verbose = FALSE)
+
+ggplot(rye.final, aes(x=Longitude, y=Latitude, color=model.spatial$residuals$values)) + 
+  geom_point() +
+  scale_color_viridis_c(option = "F") +
+  theme_bw() +
+  labs(color = "Eigenvalue") +
+  ggtitle("Variable: Residuals") + 
+  ggplot2::theme(legend.position = "bottom")+ 
+  ggplot2::xlab("Longitude") + 
+  ggplot2::ylab("Latitude")
+
+
+#Variable Importance
+spatialRF::plot_importance( model.spatial,verbose = FALSE) + ggtitle("Spatial model")
+write.csv(x=model.spatial$variable.importance, file="NewAnalyses/SpatialRF/Rye_South AmericaVarImp.csv")
+
+#Response Curves
+reponse.curves.df <- spatialRF::get_response_curves(model.spatial,variables = c("AET_mean", "rye_Fertilizer", "Pesticide", "GDP_Mean"))
+spatialRF::plot_response_curves(model.spatial, quantiles = 0.5,ncol = 2,variables = c("Pesticide", "AET_mean", "rye_Fertilizer", "GDP_Mean"))
+write.csv(x=reponse.curves.df,file="NewAnalyses/SpatialRF/Rye_South America_RespCurv.csv")
+
