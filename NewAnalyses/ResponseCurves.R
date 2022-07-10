5735124c3e9a1125c30160f351759b27bd22acd8
thecrobe <justin618s@gmail.com>
2022-07-10 09:30:03 +0100

    C

--- NewAnalyses/ResponseCurves.R
+++ NewAnalyses/ResponseCurves.R
@@ -1,88 +1,80 @@
-library(ggplot2)
-library(dplyr)
-library(ggsci)
-library(viridis)
+library(ggplot2) #plots 
+library(ggpubr) #plots
+library(dplyr) #data wrangling
+library(viridis) #colors
 
-
+#Set graphic theme
 theme_justin<-theme_bw() +theme(axis.line = element_line(colour = "black"),
                                 panel.grid.major = element_blank(),
                                 panel.grid.minor = element_blank(),
                                 panel.border = element_blank(),
                                 panel.background = element_blank())
-#Read in Response Curves
-data<-read.csv(file="NewAnalyses/ResponseCurves.csv", header=T)
-
-fertilizer<- data %>% filter(predictor.name == "Fertilizer" & quantile==0.5)
-summary(fertilizer)
-ggplot(fertilizer, aes(x=predictor,y=response)) +
-  facet_wrap(~Crop,scales = "free")  +
-  geom_point(aes(color=Region),alpha=0.3) +
-  theme_justin +
-  xlab("Log10 Fertilizer") +
-  ylab ("Log10 Yield") +
-  labs(color="Predictor") +
-  scale_color_npg() 
-
-pesticide<- data %>% filter(predictor.name == "Pesticide" & quantile==0.5)
-
-ggplot(pesticide, aes(x=predictor,y=response)) +
-  facet_wrap(~Crop,scales = "free")  +
-  geom_point(aes(color=Region),alpha=0.1) +
-  theme_justin +
-  xlab("Log10 Pesticide") +
-  ylab ("Log10 Yield") +
-  labs(color="Predictor") +
-  scale_color_npg() 
-
-gdp<- data %>% filter(predictor.name == "GDP" & quantile==0.5)
-
-ggplot(gdp, aes(x=predictor,y=response)) +
-  facet_wrap(~Crop,scales = "free")  +
-  geom_point(aes(color=Region),alpha=0.1) +
-  theme_justin +
-  xlab("Log10 GDP") +
-  ylab ("Log10 Yield") +
-  labs(color="Predictor") +
-  scale_color_npg() 
-
-aet<- data %>% filter(predictor.name == "Evapotranspiration" & quantile==0.5)
-
-ggplot(aet, aes(x=predictor,y=response)) +
-  facet_wrap(~Crop,scales = "free")  +
-  geom_point(aes(color=Region),alpha=0.1) +
-  theme_justin +
-  xlab("Log 10 Evapotranspiration") +
-  ylab ("Log10 Yield") +
-  labs(color="Predictor") +
-  scale_color_npg() 
 
 
-######### 
-
+# Model variable importance
 varimp<-read.csv(file="NewAnalyses/Global_VarImp.csv",header=T)
 varimp$Variable <- factor(varimp$Variable, levels=c("Fertilizer", "Evapotranspiration", "Pesticide","GDP","GCO"))
+varimp$Crop<-factor(varimp$Crop, levels=unique(varimp$Crop))
 
-
-ggplot(varimp, aes(x=Variable, y=Crop,fill=Importance)) + 
+ggplot(varimp, aes(x=Variable, y=Crop,fill=FertStandardized)) +
   geom_tile() + 
   theme_bw() + 
   theme(axis.text.x = element_text(angle = 45, hjust=1)) + 
   scale_fill_viridis_c()+
-  xlab("Driver") + ylab("Global Region") 
-
-
-######### Model Pref 
-
-pref<-read.csv(file="NewAnalyses/Model_Preformance.csv", header=T)
-
-ggplot(pref, aes(x=Crop,y=Region, fill=pref$r.squared.oob)) + 
-  geom_tile() +
-  geom_point(aes(size=pref$rmse.oob, alpha=0.5, color="#111111")) +
-  theme_bw() +
-  
-  scale_fill_viridis(option="mako")
+  xlab("Driver") + ylab("Crop") + 
+  labs(fill="Variable Importance")
 
 
 
 
+#Read in Response Curves
+data<-read.csv(file="NewAnalyses/Global_ResponseCurves.csv", header=T)
 
+fertilizer<- data %>% filter(predictor.name == "Fertilizer" & quantile==0.5)
+fertilizer.plot<-ggplot(fertilizer, aes(x=predictor,y=response)) +
+  facet_wrap(~Crop,scales = "free")  +
+  geom_point() +
+  theme_justin +
+  xlab("Log10 Fertilizer") +
+  ylab ("Log10 Yield") +
+  labs(color="Predictor") +
+  labs(title = "Fertilizer Response Curves")
+
+pesticide<- data %>% filter(predictor.name == "Pesticide" & quantile==0.5)
+pesticide.plot<-ggplot(pesticide, aes(x=predictor,y=response)) +
+  facet_wrap(~Crop,scales = "free")  +
+  geom_point() +
+  theme_justin +
+  xlab("Log10 Pesticide") +
+  ylab ("Log10 Yield") +
+  labs(color="Predictor") +
+  labs(title = "Pesticide Response Curves")
+
+gdp<- data %>% filter(predictor.name == "GDP" & quantile==0.5)
+gdp.plot<-ggplot(gdp, aes(x=predictor,y=response)) +
+  facet_wrap(~Crop,scales = "free")  +
+  geom_point() +
+  theme_justin +
+  xlab("Log10 GDP") +
+  ylab ("Log10 Yield") +
+  labs(color="Predictor") +
+  labs(title = "GDP Response Curves")
+
+
+aet<- data %>% filter(predictor.name == "Evapotranspiration" & quantile==0.5)
+
+aet.plot<-ggplot(aet, aes(x=predictor,y=response)) +
+  facet_wrap(~Crop,scales = "free")  +
+  geom_point() +
+  theme_justin +
+  xlab("Log10 Evapotranspiration") +
+  ylab ("Log10 Yield") +
+  labs(color="Predictor") +
+  labs(title = "Evapotranspiration Response Curves")
+
+
+ggsave(plot = gdp.plot,
+       dpi = 300,
+       path = "../Figures/",
+       filename = "GDP_ResponseCurve.tiff"
+       ,width = 8,height = 5)
