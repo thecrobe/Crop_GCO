932ceec210db2529be0339a1a774adc5e5bbe671
thecrobe <justin618s@gmail.com>
2022-11-29 09:53:57 +0100

    sim yield varimp fix

--- NewAnalyses/SimulatedYields/Cassava_SimSummary.R
+++ NewAnalyses/SimulatedYields/Cassava_SimSummary.R
@@ -1,13 +1,43 @@
 library(tidyverse)
 library(cowplot)
-data<-read.csv(file="/Users/justinstewart/Downloads/VarImp_Simulation.csv")
+library(ggpubr)
 
+#Import data
+data<-read.csv(file="NewAnalyses/SimulatedYields/ArtificialYields/Cassava_SimSummary_data.csv")
+data<-read.csv(file="NewAnalyses/SimulatedYields/ArtificialYields/Cassava_SimSummary_data.csv")
+
+#Subset GCO 
 gco<-dplyr::filter(data,Variable == "GCO")
 
+#Variable Importance
+ggplot(gco, aes(x=log10(Inflation.Factor),y=log10((Standardized.Importance)))) + # Plot standard to fertilizer
+  stat_smooth(method="loess", color="grey", size=1.5) + 
+  #geom_vline(xintercept = log10(1.08), color="#E47719", size=1.5) + # GDP
+  #geom_vline(xintercept = log10(1.02), color="#0090D1", size=1.5) + # AET
+  #geom_vline(xintercept = log10(3), color="#0090D1", size=1.5) + # Pesticide
+  geom_point(size=3) +
+  annotate(xmin = 0, xmax = log10(1.05), 
+           ymin = -Inf, ymax = Inf, 
+           geom = "rect", alpha = 0.5, fill="red") +
+  xlab("Log10 Yield Inflation Factor") + 
+  ylab("Log10 GCO Importance Relative To Fertilizer")+ 
+  scale_color_jco() +
+  theme_cowplot(12)
 
-ggplot(gco, aes(x=Simulation,y=(FertStandard), color=Variable)) + 
-  geom_point(size=2) + 
-  stat_smooth(method="loess",se = FALSE) + 
-  xlab("Yield Inflation Factor") + 
-  ylab("GCO Variable Importance") + 
+#Variable Importance
+ggplot(gco, aes(x=log10(Inflation.Factor),y=log10((Standardized.Importance)))) + # Plot standard to fertilizer
+  annotate(xmin = 0, xmax = log10(1.5), 
+           ymin = -Inf, ymax = Inf, 
+           geom = "rect", alpha = 0.4, fill="#0090D1") + 
+  geom_vline(xintercept = log10(1.08), color="black", size=1.5) + # GDP
+  annotate("text", x = .12, y = .5, label = "GDP", color="black",fontface="bold") +
+  geom_vline(xintercept = log10(1.02), color="black", size=1.5) + # AET
+  annotate("text", x = -.09, y = .3 , label = "AET",fontface="bold") +
+  geom_vline(xintercept = log10(3), color="black", size=1.5) + # Pesticide
+  annotate("text", x = .63, y = .7 , label = "Pesticide",fontface="bold") +
+  xlab("Log10 Yield Inflation Factor") + 
+  ylab("Log10 GCO Importance Relative To Fertilizer")+ 
+  stat_smooth(method="gam", color="grey", size=1.5) + 
+  geom_point(size=3) +
+  scale_color_jco() +
   theme_cowplot(12)
